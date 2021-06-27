a7e58bc7dc2d9c031dbea2efc30a849aca4370fe
Marcel Thomas <73349327+PriXss@users.noreply.github.com>
2021-06-27 22:43:19 +0200

    Update IndexLogin.js
    Little Fix

--- VerteilteSystemeProjekt/VerteilteSystemeLogIn/src/main/Website/indexlogin/IndexLogin.js
+++ VerteilteSystemeProjekt/VerteilteSystemeLogIn/src/main/Website/indexlogin/IndexLogin.js
@@ -1,5 +1,5 @@
 
 function redirectToIndex(){
-    window.location.assign("http://localhost:63342/VerteilteSysteme/src/main/Website/index/index.html?_ijt=8mla3gnajsb2gdtc9ge2b80oatdb9ro")
+    window.location.assign("http://localhost:63342/VerteilteSystemeForum/src/main/Website/index/index.html?_ijt=8mla3gnajsb2gdtc9ge2b80oatdb9ro")
 }
 
